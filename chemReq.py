56ccf7617f8ad2a32fe97bf8383056880991218c
skllpr <46583278+skllpr@users.noreply.github.com>
2020-05-23 16:35:09 -0400

    Cleaned up chemReq
    Molecule no longer shows up

--- chemReq.py
+++ chemReq.py
@@ -1,14 +1,11 @@
 from PIL import Image
 import requests
 from io import BytesIO
-def getImage(molecule):
+def getMoleculeImage(molecule):
      link = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/%s/PNG"
      link = link % molecule
      print(link)
      response = requests.get(link)
      img = Image.open(BytesIO(response.content))
      print(img)
-     img.show()
      return img
-
-getImage('benzene')
